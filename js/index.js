f6dfbfa33469d9504e962eee9f55952df8a4cb7b
reenadevi707077 <73379707+reenadevi707077@users.noreply.github.com>
2020-10-24 12:02:24 +0530

    files

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function reenaDeviFacebook() {
+    window.location.href('https://facebook.com');
+    
+}
+
+function reenaDeviTwitter() {
+    window.location.href('https://twitter.com');
+    
+}
